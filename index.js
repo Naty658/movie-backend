8b77aa12cdfae5e434e51f0c20a81520ac482d5f
Naty658 <natyfeleke@gmail.com>
2025-04-02 19:24:40 -0400

    global cors middleware added

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ const app = express();
 const PORT = 5000;
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+
 
 app.use(bodyParser.json({ limit: '2mb' }));
 
